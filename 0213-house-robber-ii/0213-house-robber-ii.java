7db64e892856dd2ac200b4f7819b44dec82e616f
Muskan <muskandev2006@gmail.com>
2026-07-11 12:52:58 +0530

    Time: 0 ms (100%), Space: 42.8 MB (60.27%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,29 +1,34 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if(n==1){
+        if(n == 1){
             return nums[0];
         }
-        int []dp1 = new int [n+1];
-        Arrays.fill(dp1,-1);
-        int []dp2 = new int [n+1];
-          Arrays.fill(dp2,-1);
-        int take = solve(nums,0,n-2,dp1);
-        int nottake = solve(nums,1,n-1,dp2);
-
-        return Math.max(take,nottake);
+       int prev = nums[0];
+       int prev2 = 0;
+       for(int i=1; i<n-1; i++){
+            int take = nums[i];
+            if( i > 1){
+                take += prev2;
+            }
+            int nottake = prev;
+            int curri = Math.max(take, nottake);
+            prev2 = prev;
+            prev = curri;
+       }
+       int prev1 = nums[1];
+       int prev3 = 0;
+       for(int i=2; i<n; i++){
+            int take = nums[i];
+            if( i > 1){
+                take += prev3;
+            }
+            int nottake = prev1;
+            int curri = Math.max(take, nottake);
+            prev3 = prev1;
+            prev1= curri;
+       }
+       return Math.max(prev,prev1);
     }
-    public int solve(int []nums, int i, int end, int []dp){
-        if(i >end){
-            return 0;
-        }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int take = nums[i] + solve(nums, i+2,end,dp);
-        int skip = solve(nums,i+1,end,dp);
-
-        dp[i] =  Math.max(take,skip);
-        return dp[i];
-    }
+    
 }
