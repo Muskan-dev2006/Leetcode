8fce4435f6d29ffaf025c103757d547277893431
Muskan <muskandev2006@gmail.com>
2026-07-12 12:44:37 +0530

    Time: 2 ms (99.79%), Space: 47.1 MB (33.18%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -10,20 +10,15 @@ class Solution {
             points[num] += num;
         }
         
-        return solve(max, points);
+        int prev1 = 0;
+        int prev2 = points[1];
+        int curri = 0;
+        for(int i = 2; i<=max;i++){
+            curri = Math.max(points[i]+prev1, prev2);
+            prev1 = prev2;
+            prev2 = curri;
+        }
+        return prev2;
     }
 
-    public int solve(int i, int []points){
-        int []dp = new int[i + 1];
-        dp[0] = 0;
-        dp[1] = points[1];
-        int take = 0;
-        int nottake = 0;
-        for(int k=2; k<=i; k++){
-            take = points[k] + dp[k-2];
-            nottake = dp[k-1];
-            dp[k] = Math.max(take, nottake);
-        }
-        return dp[i];
-    }
 }
