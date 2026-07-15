8db62ba3af07458bd67c63f2838832de708368d8
Muskan <muskandev2006@gmail.com>
2026-07-15 10:33:34 +0530

    Time: 5 ms (26.11%), Space: 50.2 MB (55.98%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -4,33 +4,27 @@ class Solution {
         int n = grid[0].length;
 
         int [][]dp = new int[m][n];
-        for(int []rows : dp){
-            Arrays.fill(rows, -1);
+        dp[0][0] = grid[0][0];
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i == 0 && j==0){
+                    dp[i][j] = grid[0][0];
+                }
+                else{
+                   int up = (int)1e9;
+                   int left = (int)1e9;
+                    if( i > 0){
+                        up = dp[i-1][j];
+                    }
+                    if( j > 0){
+                        left = dp[i][j-1];
+                    }
+                    dp[i][j] = grid[i][j] + Math.min(up, left);
+                }
+            }
         }
-        return solve(m-1, n-1,grid,dp);
+        return dp[m-1][n-1];
     }
-    public int solve(int x, int y, int [][]grid,int [][]dp){
-        if(x == 0 && y==0){
-            return grid[0][0];
-        }
-        if(x < 0){
-            return Integer.MAX_VALUE;
-        }
-        if(y < 0){
-            return Integer.MAX_VALUE;
-        }
-        if(dp[x][y]!=-1){
-            return dp[x][y];
-        }
-        int up = solve(x-1, y, grid,dp);
-        int left = solve(x, y-1, grid,dp);
-
-        int ans = Math.min(up,left);
-
-        if(ans == Integer.MAX_VALUE){
-            return dp[x][y]= Integer.MAX_VALUE;
-        }
-
-        return dp[x][y] = grid[x][y] + ans;
-    }
+    
 }
