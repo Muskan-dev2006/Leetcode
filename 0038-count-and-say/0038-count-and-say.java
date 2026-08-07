fb40017e2e939699fd8f230a3fb8380abc50adf6
Muskan <muskandev2006@gmail.com>
2026-08-07 16:47:34 +0530

    Time: 7 ms (66.51%), Space: 42.8 MB (92.9%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String countAndSay(int n) {
+        String prev="1";
+        if(n == 1){
+            return prev;
+        }
+        for(int i=2; i<=n;i++){
+            StringBuilder sb = new StringBuilder();
+            int k = 0;
+            while(k < prev.length()){
+                char ch = prev.charAt(k);
+                int count = 0;
+                while(k < prev.length() && prev.charAt(k) == ch){
+                    count++;
+                    k++;
+                }
+
+                sb.append(count);
+                sb.append(ch);
+            }
+            prev = sb.toString();
+        }
+        return prev;
+    }
+}
