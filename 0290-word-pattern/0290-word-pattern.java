ea233f8b32f8cb643dc2ee3bf8f311a73b696270
Muskan <muskandev2006@gmail.com>
2026-08-14 11:19:01 +0530

    Time: 1 ms (83.78%), Space: 42.5 MB (88.04%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] ans = s.split(" ");
+
+        if (pattern.length() != ans.length) {
+            return false;
+        }
+
+        HashMap<Character, String> map = new HashMap<>();
+        HashSet<String> used = new HashSet<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+
+            char ch = pattern.charAt(i);
+            String word = ans[i];
+
+            if (map.containsKey(ch)) {
+
+                
+                if (!map.get(ch).equals(word)) {
+                    return false;
+                }
+
+            } else {
+
+           
+                if (used.contains(word)) {
+                    return false;
+                }
+
+                map.put(ch, word);
+                used.add(word);
+            }
+        }
+
+        return true;
+    }
+}
