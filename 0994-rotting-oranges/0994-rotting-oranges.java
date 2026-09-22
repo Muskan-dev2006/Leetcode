3bdab58678cb98e0d1889a16437bdcb9c71f56ad
Muskan <muskandev2006@gmail.com>
2026-09-22 11:00:56 +0530

    Time: 84 ms (3.15%), Space: 44.1 MB (71.59%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,64 +1,58 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        
-        if(grid.length == 0){
-            return 0;
-        }
+        if(grid.length == 0) return 0;
+
         int m = grid.length;
         int n = grid[0].length;
 
         int total = 0;
         int count = 0;
 
-        Queue<int[]>rotten = new LinkedList<>();
+        Queue<int []>q = new LinkedList<>();
         for(int i=0; i<m; i++){
-            for(int j= 0; j<n; j++){
-
-                if(grid[i][j] !=0) total++;
-
+            for(int j=0; j<n; j++){
+                if(grid[i][j]!=0){
+                    total ++;
+                }
                 if(grid[i][j] == 2){
-                    rotten.add(new int []{i,j});
+                    q.add(new int [] {i,j});
                 }
             }
         }
-        int []dx = {0,0,1,-1};
+        int []dx  = {0,0,1,-1};
         int []dy = {1,-1,0,0};
 
+
         int days = 0;
+        while(!q.isEmpty()){
+            int k = q.size();
+            count += k;
+            for(int j=0; j<k; j++){
+                    int []pos = q.poll();
+            int x = pos[0];
+            int y = pos[1];
+
+            for(int i=0; i<4; i++){
+                int srcx = x + dx[i];
+                int srcy = y+ dy[i];
 
 
-        while(!rotten.isEmpty()){
-            int k = rotten.size();
-
-            count+= k;
-
-
-            for(int i=0; i<k; i++){
-
-                int []pos = rotten.poll();
-
-                int x = pos[0];
-                int y = pos[1];
-
-
-                for(int d=0; d<4; d++){
-                    int nx = x + dx[d];
-                    int ny = y + dy[d];
-
-
-                    if(nx < 0 || ny < 0 || nx >= m || ny>= n || grid[nx][ny]!=1){
-                        continue;
-                    }
-
-                    grid[nx][ny] = 2;
-
-                    rotten.add(new int []{nx, ny});
+                if(srcx < 0 || srcx >= m || srcy < 0 || srcy >= n || grid[srcx][srcy]!=1){
+                    continue;
                 }
-            }
 
-            if(!rotten.isEmpty()) days++;
+                grid[srcx][srcy] = 2;
+                q.add(new int [] {srcx, srcy});
+
+
+            }
+             
+            }
+            if(!q.isEmpty()) days++;
+            
 
         }
-        return total == count ? days : -1;
+
+        return total == count ? days :  -1;
     }
 }
